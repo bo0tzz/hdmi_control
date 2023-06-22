32610bf1754a6645fe6f54f489ecdafcdd4a82bd
bo0tzz <git@bo0tzz.me>
2023-06-22 16:31:57 +0200

    Add rs232 lib dep

--- matrix_controller/mix.exs
+++ matrix_controller/mix.exs
@@ -44,7 +44,8 @@ defmodule MatrixController.MixProject do
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:leaf_hdmi_matrix, path: "../leaf_hdmi_matrix"}
     ]
   end
 
